d7889cf53eb55f97e8a49dc71786ee1c37c1689a
Ayanrinde Olutayo Stephen <olutayostephen@gmail.com>
2024-04-16 10:46:33 +0100

    updated

--- controllers/user.controllers.js
+++ controllers/user.controllers.js
@@ -1,3 +1,4 @@
+const { status } = require("express/lib/response");
 const userModel = require("../models/user.model")
 let jwt = require("jsonwebtoken");
 let nodemailer = require("nodemailer");
@@ -22,5 +23,22 @@ const fetchQuestion = (req,res)=>{
     .then((result)=>{console.log("questions loaded successfully"); res.send({status:true, message:"Questions loaded successfully", result })})
     .catch((err)=>{console.log("could not load questions", err); res.send({status:false, message:"could not load questions"})})
 }
-
-module.exports={admin, fetchQuestion}
+const displayQuestions = (req,res)=>{
+    userModel.find()
+    .then((result)=>{console.log(result); res.send({status:true, result, message:"Questions loaded successfully" })})
+    .catch((err)=>{console.log("couldnt load questions" + err); res.send({status:false, maessage:"could not load questions"})})
+}
+const deleteQestion = (req,res)=>{
+    let userID = req.body._id; console.log(req.body);
+    userModel.findOneAndDelete({_id:userID})
+    .then((result)=>{console.log(result); res.send({status:true, message:"Deleted successfully", result})})
+    .catch((err)=>{console.log(err+ "couldnt delete"); res.send({status:false, message:"could not Delete", result})})
+}
+const editQuestion = (req,res)=>{ console.log(req.body);
+let question = req.body.eQuestion; let optionA = req.body.eOptionA; let optionB= req.body.eOptionB;
+let optionC = req.body.eOptionC; let optionD = req.body.eOptionD; let correctAnswer= req.body.eCorrectAnswer 
+userModel.findOneAndUpdate({_id:req.body.editID}, {question, optionA, optionB, optionC, optionD, correctAnswer})
+.then ((result)=> {res.send({status:true, message: "Edited successfully", result}); console.log("edited succesfully");})
+.catch((err)=>{res.send({status:false, message:" Edit failed" + " " + err}); console.log(err, "couldnt edit");})
+}
+module.exports={admin, fetchQuestion, displayQuestions, deleteQestion, editQuestion}
--- routes/user.route.js
+++ routes/user.route.js
@@ -1,8 +1,10 @@
 const express = require("express")
 const router = express.Router()
-const{admin, fetchQuestion} = require("../controllers/user.controllers")
+const{admin, fetchQuestion, displayQuestions, deleteQestion, editQuestion} = require("../controllers/user.controllers")
 
 router.post("/admin", admin)
 router.get("/fetchquestions", fetchQuestion)
-
+router.get("/displayQuestions", displayQuestions)
+router.post("/deleteQestion", deleteQestion)
+router.post("/editQuestion", editQuestion)
 module.exports = router
